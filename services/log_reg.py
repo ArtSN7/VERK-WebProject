f745a79419d86a4a9bcc1025e80230bea468831c
max <m7a7x7gg@gmail.com>
2023-04-08 01:43:41 +0300

    menu added, without tasks

--- services/log_reg.py
+++ services/log_reg.py
@@ -1,4 +1,4 @@
-#file with registration and login
+# file with registration and login
 
 
 import data.db_session as session
@@ -13,13 +13,10 @@ from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
 from data.users import User
 
-
 blueprint = flask.Blueprint('log_reg', __name__, template_folder='templates')
-
 
 session.global_init("db/blogs.db")
 db_session = session.create_session()
-
 
 
 class LoginForm(FlaskForm):
@@ -49,18 +46,18 @@ class RegistrationForm(FlaskForm):
 def login():
     form = LoginForm()
     db_session = session.create_session()
-    
+
     if form.validate_on_submit():
         user = db_session.query(User).filter(User.email == form.email.data).first()
         pas = user.password
-        
+
         if user and user.check_password(pas, form.password.data):
             login_user(user)
-            
-            return redirect('/main')
-        
+
+            return redirect('/base')
+
         return render_template('login.html', message='Wrong login or password', form=form)
-    
+
     return render_template('login.html', title='Login', form=form)
 
 
@@ -94,9 +91,36 @@ def registration():
     return render_template('signup.html', title='Registration', form=form)
 
 
-
 @blueprint.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect("/")
+
+
+@blueprint.route('/base')
+def base():
+    # тут имя надо брать введенное пользователем, но я даун не знаю как это сделать, а щас уже разбираться поздно да и
+    # я не соображу, поэтому чтобы без говнокода напишу заглушку. А еще нужно либо сделать выбор аватарки из
+    # предложенных, либо как в лицее основывываясь на возрасте, но тогда нужно в регистрации добавить ввод возраста
+    return render_template('base.html', title='Base', name='Billy S.')
+
+
+@blueprint.route('/agenda')
+def agenda():
+    return render_template('agenda.html', title='Agenda', name='Billy S.')
+
+
+@blueprint.route('/projects')
+def projects():
+    return render_template('projects.html', title='Projects', name='Billy S.')
+
+
+@blueprint.route('/tasks')
+def tasks():
+    return render_template('tasks.html', title='Tasks', name='Billy S.')
+
+
+@blueprint.route('/notifications')
+def notifications():
+    return render_template('notifications.html', title='Notifications', name='Billy S.')
