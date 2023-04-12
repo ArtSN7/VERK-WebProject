9869cbcb82c9bfb8ede763fc89efa4c769def39d
max <m7a7x7gg@gmail.com>
2023-04-12 20:55:38 +0300

    50% of smth

--- funcs.py
+++ funcs.py
@@ -27,8 +27,8 @@ def load_user(user_id):
 if __name__ == '__main__':
     session.global_init("db/blogs.db")
     db_session = session.create_session()
-    
-    #reg, login, logout ; after success a person will be redirected to the main (/main)
+
+    # reg, login, logout ; after success a person will be redirected to the main (/main)
     app.register_blueprint(log_reg.blueprint)
 
     app.run(port=8080, host='127.0.0.1')
--- services/log_reg.py
+++ services/log_reg.py
@@ -3,8 +3,8 @@
 
 import data.db_session as session
 from data.users import User
-from flask import Flask, redirect, render_template, request
-from flask_login import LoginManager, logout_user, login_required, login_user
+from flask import Flask, redirect, render_template, request, g
+from flask_login import LoginManager, logout_user, login_required, login_user, current_user
 import datetime
 import flask
 from wtforms import PasswordField, BooleanField, SubmitField, EmailField, SearchField, SelectField, IntegerField, \
@@ -12,6 +12,7 @@ from wtforms import PasswordField, BooleanField, SubmitField, EmailField, Search
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
 from data.users import User
+from funcs import load_user
 
 blueprint = flask.Blueprint('log_reg', __name__, template_folder='templates')
 
@@ -22,7 +23,7 @@ db_session = session.create_session()
 class LoginForm(FlaskForm):
     email = EmailField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
-    submit = SubmitField('Login')
+    submit = SubmitField('Log in')
 
 
 class RegistrationForm(FlaskForm):
@@ -54,11 +55,11 @@ def login():
         if user and user.check_password(pas, form.password.data):
             login_user(user)
 
-            return redirect('/base')
+            return redirect(f'/projects/{user.id}')
 
         return render_template('login.html', message='Wrong login or password', form=form)
 
-    return render_template('login.html', title='Login', form=form)
+    return render_template('login.html', title='Verk | Login', form=form)
 
 
 @blueprint.route('/registration', methods=['GET', 'POST'])
@@ -86,9 +87,9 @@ def registration():
         db_session.add(user)
         db_session.commit()
 
-        return redirect('/login')
+        return redirect(f'/projects/{user.id}')
 
-    return render_template('signup.html', title='Registration', form=form)
+    return render_template('signup.html', title='Verk | Registration', form=form)
 
 
 @blueprint.route('/logout')
@@ -98,29 +99,61 @@ def logout():
     return redirect("/")
 
 
-@blueprint.route('/base')
-def base():
+@blueprint.route('/base/<int:user_id>')
+def base(user_id):
     # тут имя надо брать введенное пользователем, но я даун не знаю как это сделать, а щас уже разбираться поздно да и
     # я не соображу, поэтому чтобы без говнокода напишу заглушку. А еще нужно либо сделать выбор аватарки из
     # предложенных, либо как в лицее основывываясь на возрасте, но тогда нужно в регистрации добавить ввод возраста
-    return render_template('base.html', title='Base', name='Billy S.')
+    db_sess = session.create_session()
+    user = db_sess.query(User).get(user_id)
+    print(LoginManager.user_loader)
+    return render_template('base.html', title='Verk | Base', name=user.name, id=user_id)
 
 
-@blueprint.route('/agenda')
-def agenda():
-    return render_template('agenda.html', title='Agenda', name='Billy S.')
+@blueprint.route('/agenda/<int:user_id>')
+def agenda(user_id):
+    db_sess = session.create_session()
+    user = db_sess.query(User).get(user_id)
+    return render_template('agenda.html', title='Verk | Agenda', name=user.name, id=user_id)
 
 
-@blueprint.route('/projects')
-def projects():
-    return render_template('projects.html', title='Projects', name='Billy S.')
+@blueprint.route(f'/projects/<int:user_id>')
+def projects(user_id):
+    prj_list = [{
+        "img": "https://images.unsplash.com/photo-1563089145-599997674d42?ixlib=rb-4.0.3&ixid=Mnw\
+                    xMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        "title": "Codding"
+    },
+        {
+            "img": "https://images.unsplash.com/photo-1618472609777-b038f1f04b8d?ixlib=rb-4.0.3&ixid=Mnw\
+                    xMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
+            "title": "Design"
+        },
+        {
+            "img": "https://images.unsplash.com/photo-1618556450994-a6a128ef0d9d?ixlib=rb-4.0.3&ixid=Mnw\
+            xMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
+            "title": "Marketing"
+        },
+        {
+            "img": "https://images.unsplash.com/photo-1620641788421-7a1c342ea42e?ixlib=rb-4.0.3&ixid=Mnw\
+            xMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+            "title": "Prototyping"
+        },
+    ]
+    db_sess = session.create_session()
+    user = db_sess.query(User).get(user_id)
+    return render_template('projects.html', title='Verk | Projects', name=user.name, id=user_id, projects=prj_list)
 
 
-@blueprint.route('/tasks')
-def tasks():
-    return render_template('tasks.html', title='Tasks', name='Billy S.')
+@blueprint.route('/tasks/<int:user_id>')
+def tasks(user_id):
+    db_sess = session.create_session()
+    user = db_sess.query(User).get(user_id)
+    return render_template('tasks.html', title='Verk | Tasks', name=user.name, id=user_id)
 
 
-@blueprint.route('/notifications')
-def notifications():
-    return render_template('notifications.html', title='Notifications', name='Billy S.')
+@blueprint.route('/notifications/<int:user_id>')
+def notifications(user_id):
+    db_sess = session.create_session()
+    user = db_sess.query(User).get(user_id)
+    return render_template('notifications.html', title='Verk | Notifications', name=user.name, id=user_id)
