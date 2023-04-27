60345a37586de403e90d74228fa19b83bb28fb4a
max <m7a7x7gg@gmail.com>
2023-04-27 21:39:24 +0300

    Revert "-200%"
    This reverts commit 0b9406fd

--- services/task_adding.py
+++ services/task_adding.py
@@ -1,4 +1,4 @@
-# добавление задачи
+#добавление задачи 
 
 
 import data.db_session as session
@@ -16,23 +16,25 @@ from data.tasks import Tasks
 from data.projects import Project
 import datetime
 
+
 blueprint = flask.Blueprint('adding_task', __name__, template_folder='templates')
 
 session.global_init("db/blogs.db")
 db_session = session.create_session()
 
 
+
 def checking_users(users):
     db_session = session.create_session()
     answer = []
-
+    
     for i in users.split(', '):
         user = db_session.query(User).filter(User.email == i).first()
         if not user.id:
             return []
         else:
             answer.append(user.id)
-
+            
     return answer
 
 
@@ -42,45 +44,48 @@ def checking_users_in_pr(check, pr_id):
     for i in check:
         if i not in pr_users:
             return True
-
-    return False
+    
+    return False        
 
 
 class AddingTaskForm(FlaskForm):
+    
     description = StringField('Description', validators=[DataRequired()])
-
-    # img = SelectField('Image', validators=[DataRequired()], choices=[('1', 'cat'), ('2', 'dog'), ('3', 'cow')])
-
+    
+    #img = SelectField('Image', validators=[DataRequired()], choices=[('1', 'cat'), ('2', 'dog'), ('3', 'cow')])
+    
     users = StringField("Users' emails separated by commos and spaces", validators=[DataRequired()])
-
+    
     end_date = DateTimeField("End date", validators=[DataRequired()], format="%Y-%m-%d")
-
+    
     submit = SubmitField('ADD')
-
-
-@blueprint.route('/adding_task/<int:user_id>', methods=['GET', 'POST'])
-def adding_task(user_id):
+    
+    
+@blueprint.route('/adding_project/<int:user_id>/<int:project_id>', methods=['GET', 'POST'])
+def adding_project(user_id, project_id):
     form = AddingTaskForm()
     db_session = session.create_session()
 
     if form.validate_on_submit():
-
+        
         check = checking_users(form.users)
-
+        
         if check == []:
             return render_template('adding_project.html', title='Adding Task', form=form,
                                    message="Some users were not found")
-
-        if checking_users_in_pr(check):
+        
+        if checking_users_in_pr(check, project_id):
             return render_template('adding_project.html', title='Adding Task', form=form,
                                    message="Some users are not in the project")
-
+        
         if len(check) == 1:
             answ = check[0]
         else:
             answ = ', '.join(check)
 
+
         task = Tasks(
+            project=project_id,
             description=form.description.data,
             start_date=datetime.date.today(),
             end_date=form.end_date.data,
